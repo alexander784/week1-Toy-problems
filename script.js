789b62ea79c8bc59fb44b9b74b16a70c9b13ffa9
alexander <alexandernyaga1999@gmail.com>
2023-09-23 20:46:48 +0300

    condition to check grade

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ function calculateGrade(){
     let Marks = parseFloat(prompt(`Enter student marks (between 0 and 100):`));
 // Declare grade variable
 let grade;
+// Check marks against Grade ranges and assign the corresponding Grade
+if (marks > 79){
+    grade = "A";
+}else if (marks>= 60){
+    grade = "B"
+}else if (marks >= 50) {
+    grade = "C"
+}else if(marks>= 40) {
+    grade ="D"
+
+}else{
+    grade = "E"
+}
 
 }
 
