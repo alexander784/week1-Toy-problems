4b038b95c84d7c4034d234cbfc5de58c7bdad430
alexander <alexandernyaga1999@gmail.com>
2023-09-23 20:51:04 +0300

    condition within speed limit

--- script.js
+++ script.js
@@ -32,6 +32,14 @@ const speedLimit = 70;
 let demeritPoints = 0;
 //Get speed Difference
 const speedDifference = speed - speedLimit;
+//Check whether speed is within the limit
+if(speedDifference <= 0) {
+    console.log("Ok");
+}else{
+    //Calculation of demeritspoints
+    demeritPoints = Math.floor(speedDifference / 5);
+
+    console.log("Points", demeritPoints);
 
 
 
