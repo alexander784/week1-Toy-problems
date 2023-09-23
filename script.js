575a53f8b47ad3b61d16169c28ef0755fda04bd2
alexander <alexandernyaga1999@gmail.com>
2023-09-23 20:45:54 +0300

    declare grade variable

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // Define the function
 function calculateGrade(){
+
+    //input Student Marks
+    let Marks = parseFloat(prompt(`Enter student marks (between 0 and 100):`));
+// Declare grade variable
+let grade;
+
 }
 
     
