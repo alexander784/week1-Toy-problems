3206ddd3cf1c66ca627f02b4caf574cb393b9cfe
alexander <alexandernyaga1999@gmail.com>
2023-09-23 06:26:27 +0300

    check speed within limit

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ let demeritPoints = 0;
 
 //Get speed Difference
 const speedDifference = speed - speedLimit;
+
+//Check whether speed is within the limit
+if(speedDifference <= 0) {
+    console.log("Ok");
+}
